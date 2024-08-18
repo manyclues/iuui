3cc548292bce7f0c99535d9c22eda2693e23df06
Khaled Nadam <khalednadam@gmail.com>
2024-08-18 17:52:02 +0300

    fix: :bug: export types

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true })],
   build: {
+    // library entry and output settings
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       name: "iuui",
       fileName: "iuui",
     },
-    //for bundle
+    // bundler options
+    // externalize react-related imports
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
