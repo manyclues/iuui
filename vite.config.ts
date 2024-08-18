08745492d7dc6814df4dca98c2d619b0ba562d27
Khaled Nadam <khalednadam@gmail.com>
2024-08-18 19:57:39 +0300

    feat: :sparkles: export styles

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,26 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true })],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({ rollupTypes: true }),
+    libInjectCss(),
+  ],
   build: {
     // library entry and output settings
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      name: "iuui",
-      fileName: "iuui",
+      entry: [
+        resolve(__dirname, "lib/main.ts"),
+        resolve(__dirname, "lib/IUButton/IUButton.tsx"),
+        resolve(__dirname, "lib/IUTextField/IUTextField.tsx"),
+      ],
+      // name: "iuui",
+      // fileName: "iuui",
     },
     // bundler options
     // externalize react-related imports
