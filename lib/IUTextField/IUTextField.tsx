118097f35ec0d9cb98eb198c96383bc454b39580
codecret <malekkhodary@hotmail.com>
2024-08-19 03:36:03 +0300

    feat: :sparkles: add root styles for textfield

--- lib/IUTextField/IUTextField.tsx
+++ lib/IUTextField/IUTextField.tsx
@@ -1,8 +1,52 @@
 import { ReactNode } from "react";
 import "./iu-textfield.css";
+import { cva } from "class-variance-authority";
 
-const IUTextField = (): ReactNode => {
-  return <input type="text" className="iu_textfield" />;
+interface TextFieldProps {
+  variant?: "outline" | "unstyled" | "flushed" | "filled";
+  size?: "xs" | "sm" | "md" | "lg";
+  placeholder?: string;
+  fieldType?: "text" | "email" | "password" | "number";
+  borderColor?: string;
+}
+const textFieldStyles = cva("iu_textfield", {
+  variants: {
+    variant: {
+      outline: "iu_outline",
+      unstyled: "iu_unstyled",
+      flushed: "iu_flushed",
+      filled: "iu_filled",
+    },
+    size: {
+      xs: "iu_xsmall",
+      sm: "iu_small",
+      md: "iu_medium",
+      lg: "iu_large",
+    },
+  },
+  defaultVariants: {
+    variant: "outline",
+    size: "md",
+  },
+});
+
+const IUTextField = ({
+  variant,
+  size,
+  placeholder,
+  fieldType,
+  borderColor,
+}: TextFieldProps): ReactNode => {
+  return (
+    <input
+      type={fieldType || "text"}
+      className={textFieldStyles({ variant, size })}
+      placeholder={placeholder ? placeholder : ""}
+      style={{
+        borderColor: borderColor || "initial",
+      }}
+    />
+  );
 };
 
 export default IUTextField;
